7465fa093bfcad155b2fd322d6462c93648ba955
S-Luther <samluther998@gmail.com>
2022-10-03 16:51:53 -0600

    This is the code copied down from canvas Week 4/game.c

--- C/TwoDGame.c
+++ C/TwoDGame.c
@@ -1,98 +1,82 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-
-int main(int argc, char* argv[]) {
-
-    char map[20][100] = { 0 };
-    char *p = "P";
-    map[2][2] = &p; //ERROR
-    int proceed = 0; //true
-    int argsCheck = 1; //false
-
-    int argsRunIndex = 0;
-
-    const char* directions[4] = {"North", "East", "South", "West"};
-    int directionX[4] = {0, -1, 0, 1};
-    int directionY[4] = {1, 0, -1, 0};
-
+int main( int argc, char *argv[] ) {
+    int proceed = 1;
+    int map[100][20] = { 0 }; 
+    const char *directions[4] = {"North", "East", "South", "West"};
+    int directionX[4]         = {      0,      1,       0,     -1}; 
+    int directionY[4]         = {     -1,      0,       1,      0}; 
+    int currentPosition[] = {0, 10, 10};
     int currentDirection = 0;
-
-    int currentPosition[] = {2,2};
-
-    if(argc > 0) {
-        argsCheck = 0; //true
+    int player = 2;
+    int path = 1;
+    int argsCheck = 0;
+    int argsIndex = 0;
+    map[10][10] = player;
+    if(argc > 0){
+        argsCheck = 1;
     }
-    while (proceed) {
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 100; j++) {
-                if (map[i][j] == NULL) { //ERROR
-                    printf(" ");
-                } else {
-                    printf(map[i][j]); //ERROR
+    while(proceed == 1){
+        char input[1];
+        for(int y = 0; y<20; y++){
+            for(int x = 0; x<100; x++){
+                if(map[x][y] == 0){
+                    printf("|");
+                }else if (map[x][y] == 2){
+                    printf("P");
+                }
+                else{
+                    printf("#");
                 }
             }
-            printf("\n"); //Will print new line after each row
+            printf("\n");
         }
-        printf("Please traverse the world and explore using these commands: \n  W - Walk Forward\n  A - Turn Left\n  D - Turn Right\n  E - Check Direction\n  Q - Quit the Game");
-
-        char input;
-
-        if (!argsCheck) {
-            scanf("%s", input); 
-        } else {
-            if (argsRunIndex == argc) {
-                argsCheck = 1; //false
-                input = "e"; //ERROR
-            } else {
-                input = *argv[argsRunIndex];
-                argsRunIndex++;
+        printf("Please traverse the world and explore using these commands: \n  W - Walk Forward\n  A - Turn Left\n  D - Turn Right\n  E - Check Direction\n  Q - Quit the Game\n");
+        if(argsCheck == 0){ 
+            scanf("%s", input);
+        }else{
+            if(argc == argsIndex){
+                argsCheck = 0;
+                *input = *"e";
+            }else{
+                *input = *argv[argsIndex];
+                argsIndex++;
             }
-
-        
         }
-        switch(input){
-                case 'w':
-                    map[currentPosition[1]][currentPosition[0]] = "#"; //ERROR
-                    //saves the trail of where you have been
-                    if(!((currentPosition[0] - directionX[currentDirection] < 0) || (currentPosition[0] - directionX[currentDirection] > 100)||(currentPosition[1] - directionY[currentDirection] < 0) || (currentPosition[1] - directionY[currentDirection] > 20))){
-                        //checks if you're bumping into the edge
-                        currentPosition[0] = currentPosition[0] - directionX[currentDirection];
-                        currentPosition[1] = currentPosition[1] - directionY[currentDirection];
-                       
-                    }
-
-                    map[currentPosition[1]][currentPosition[0]] = "P"; //ERROR
-                    //puts your player icon down
-                    break;
-                case 'a':
-                    currentDirection--;
-                    //moves your index up or down the 3 arrays listed above
-                    if(currentDirection < 0){
-                        currentDirection = 3;
-                    }
-                    break;
-                case 'd':
-                    currentDirection++;
-                    //moves your index up or down the 3 arrays listed above
-
-                    if(currentDirection > 3){
-                        currentDirection = 0;
-                    }
-                    break;
-                case 'e':
-                    printf("You are facing in the %i ward direction!", directions[currentDirection]);
-                    break;
-                case 'q':
-
-                    proceed = 0; //true
-                    break;
+        switch(input[0]){
+            case 'w' :
+                map[currentPosition[1]][currentPosition[2]] = path;
                 
-                default:
-                    printf("You must enter one of the commands!");
-
-            }
+                if(!((currentPosition[1] + directionX[currentDirection] < 0) || 
+                    (currentPosition[1] + directionX[currentDirection] > 100))){
+                    currentPosition[1] = currentPosition[1] + directionX[currentDirection];
+                }
+                if(!((currentPosition[2] + directionY[currentDirection] < 0) || 
+                        (currentPosition[2] + directionY[currentDirection] > 20))){
+                    currentPosition[2] = currentPosition[2] + directionY[currentDirection];
+                }
+                map[currentPosition[1]][currentPosition[2]] = player;
+                break;
+            case 'a' :
+                currentDirection--;
+                if(currentDirection < 0){
+                    currentDirection = 3;
+                }
+                break;
+            case 'd' :
+                currentDirection++;
+                if(currentDirection > 3){
+                    currentDirection = 0;
+                }
+                break;
+            case 'e' :
+                printf("Your current direction is %s! \n", directions[currentDirection]);
+                break;
+            case 'q' :
+                proceed = 0;
+                break;
+            default: 
+                printf("Please enter valid command!\n");
+        }
     }
-
     return 0;
 }
